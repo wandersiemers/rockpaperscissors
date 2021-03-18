c49877e74c60f9653f469bce82485b984e99cc3e
Wander Siemers <wandersiemers@me.com>
2021-03-18 16:59:41 +0100

    Initial commit

---
+++ rps.py
@@ -0,0 +1,102 @@
+from random import randrange
+import time
+
+# Use unicode support in most terminals to boldify text
+def print_bold(str):
+	print("\033[1m" + str + "\033[0m")
+
+class Game:
+
+	score_pc = score_user = 0
+
+	def play_round(self):
+		print("Press R for Rock, P for Paper or S for Scissors")
+		s = input().casefold()
+
+		print("3...")
+		time.sleep(1)
+		print("2...")
+		time.sleep(1)
+		print("1...")
+		time.sleep(1)
+		self.handle_move(s, self.generate_pc_move(True))
+
+	def handle_move(self, move_a, move_b):
+		win_index = -1
+
+		options = ["r", "p", "s"] 
+		if move_a not in options or move_b not in options:
+			print(f"Invalid pick '{move_a}', try again")
+			play_round()
+
+		if move_a == "r":
+			if move_b == "r":
+				win_index = 2
+			elif move_b == "p":
+				win_index = 1
+			elif move_b == "s":
+				win_index = 0
+		elif move_a == "p":
+			if move_b == "r":
+				win_index = 0
+			elif move_b == "p":
+				win_index = 2
+			elif move_b == "s":
+				win_index = 1
+		elif move_a == "s":
+			if move_b == "r":
+				win_index = 1
+			elif move_b == "p":
+				win_index = 0
+			elif move_b == "s":
+				win_index = 2
+		else:
+			print(f"Unknown move {move_a}")
+			
+
+		self.process_move_result(win_index)
+
+	def process_move_result(self, outcome):
+		if outcome == 0:
+			print_bold("You beat the computer!")
+			self.score_user += 1
+		elif outcome == 1:
+			print_bold("Computer won this round.")
+			self.score_pc += 1
+		elif outcome == 2:
+			print_bold("Tie!")
+		else:
+			print_bold("invalid")
+
+		print(f"Score: You {self.score_user} - {self.score_pc} Computer")
+
+
+	# Starts a new round or quits, printing the final score
+	def start_game(self):
+		print_bold("Press P to play or Q to quit")
+		input_str = input()
+		casefolded = input_str.casefold() # a variant of .lower() which handles edge cases better
+		
+		if casefolded == "p":
+			self.play_round()
+		elif casefolded == "q":
+			print(f"Final score: You {self.score_user} - {self.score_pc} Computer")
+			exit()
+		else:
+			print(f"Unknown input {input_str}")
+			self.start_game()
+
+	def generate_pc_move(self, print_move = True):
+		move = ["r", "p", "s"][randrange(2)]
+		if print_move:
+			print(f"Computer picked {move.upper()}")
+		return move
+
+def main():
+		print("Welcome to Rock Paper Scissors!")
+		game = Game()
+		while True:
+			game.start_game()
+
+if __name__ == "__main__":
+		main()
